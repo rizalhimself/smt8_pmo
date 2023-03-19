5df392877ca12a6fc2c8a4ab1ede1298208d7d09
rizalhimself <rizalhimself@gmail.com>
2023-03-19 14:20:58 +0700

    coding splash screen jadi

---
+++ app/src/main/java/com/example/pmo23_praktikum/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.pmo23_praktikum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity2.this, MainActivity.class);
+
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
