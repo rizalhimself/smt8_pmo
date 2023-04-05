d12cad9dbe03a9e9891266d48fdd2e1133db475f
rizalhimself <rizalhimself@gmail.com>
2023-04-05 15:20:55 +0700

    menambahkan fungsi button pada activity login

--- app/src/main/java/com/example/pmo23_praktikum/LoginActivity.java
+++ app/src/main/java/com/example/pmo23_praktikum/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.pmo23_praktikum;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button btn_Login = findViewById(R.id.btLogin);
+        btn_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
     }
 }
